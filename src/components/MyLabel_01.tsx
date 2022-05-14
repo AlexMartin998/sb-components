30774fffe6ea019d35b29bbe604851f3f156dac6
Alex Martin <alextravc.342@gmail.com>
2022-05-14 16:35:39 -0500

    [ADD]: storybook

---
+++ src/components/MyLabel_01.tsx
@@ -0,0 +1,45 @@
+import './mylabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Do you want to capitalize the label?
+   */
+  allCaps: boolean;
+
+  /**
+   * What text color to use
+   */
+  color: 'primary' | 'secondary' | 'tertiary';
+
+  /**
+   * Font Color
+   */
+  fontColor?: string;
+
+  /**
+   * Label contents
+   */
+  label: string;
+
+  /**
+   * How large should the button be?
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+}
+
+export const MyLabel = ({
+  allCaps,
+  color,
+  label = 'No label',
+  size = 'normal',
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: `${fontColor}` }}
+    >
+      {(allCaps && label.toUpperCase()) || label}
+    </span>
+  );
+};
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,51 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { MyLabel } from '../../components/MyLabel_01';
+
+// // Our story
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  argTypes: {
+    color: { control: 'select', options: ['primary', 'secondary', 'tertiary'] },
+    fontColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = args => <MyLabel {...args} />; // Enabling Args in Storybook
+
+// // Crear las pages/cuentos de la Story  <- .bind({})  romper ref   this
+export const Basic = Template.bind({});
+
+// Args with Default values in Storybook
+Basic.args = {
+  allCaps: true, // true - capitalize all word
+  size: 'h1',
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  allCaps: true,
+  size: 'normal',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  allCaps: false,
+  size: 'normal',
+  color: 'secondary',
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  allCaps: false,
+  size: 'normal',
+  color: 'tertiary',
+};
+
+export const CustomFormColor = Template.bind({});
+CustomFormColor.args = {
+  allCaps: false,
+  size: 'h1',
+  fontColor: '#5517ac',
+};
